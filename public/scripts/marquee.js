29beef46cb970e9c9869ea14d6da33d950e325e4
Widenyo <misterhomunculo@gmail.com>
2022-11-01 19:34:15 -0300

    gif container

---
+++ public/scripts/marquee.js
@@ -0,0 +1,12 @@
+marqueeInit({
+	uniqueid: 'mycrawler',
+	style: {
+        'width': '322px'
+	},
+	inc: 5, //speed - pixel increment for each iteration of this marquee's movement
+	mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+	moveatleast: 2,
+	neutral: 150,
+	persist: true,
+	savedirection: true
+});
