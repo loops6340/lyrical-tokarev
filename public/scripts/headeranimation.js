02c510a815add1ab6bfcf47d0bcd80545f06e119
Widenyo <misterhomunculo@gmail.com>
2022-11-01 17:10:35 -0300

    index

--- public/scripts/headeranimation.js
+++ public/scripts/headeranimation.js
@@ -1,13 +1,23 @@
-const topSprites = document.getElementsByClassName('top-sprite')
+const topSprites = document.getElementsByClassName("top-sprite");
 
-for(let i = 0; i < topSprites.length; i++){
-    const sprite = topSprites[i]
-    sprite.addEventListener('mouseover', () => {
-        sprite.children[1].style.color = 'yellow'
-        sprite.style.animationPlayState = 'paused';
-    })
-    sprite.addEventListener('mouseout', () => {
-        sprite.children[1].style.color = 'white'
-        sprite.style.animationPlayState = 'running';
-    })
-}
+setTimeout(() => {
+  for (let i = 0; i < topSprites.length; i++) {
+    const sprite = topSprites[i];
+    sprite.addEventListener("mouseover", () => {
+      sprite.children[1].style.color = "yellow";
+      sprite.style.animationPlayState = "paused";
+      if (sprite.children[1].innerHTML === "GUESTBOOK") {
+        sprite.children[0].src =
+          "/public/images/headersprites/hover/alicemssprite.gif";
+      }
+    });
+    sprite.addEventListener("mouseout", () => {
+      sprite.children[1].style.color = "white";
+      sprite.style.animationPlayState = "running";
+      if (sprite.children[1].innerHTML === "GUESTBOOK") {
+        sprite.children[0].src =
+          "/public/images/headersprites/alicemssprite.png";
+      }
+    });
+  }
+}, 6100);
