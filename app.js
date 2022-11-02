9c85238d7296c4ce659636a8b7dd4c2588078aa6
Widenyo <misterhomunculo@gmail.com>
2022-11-02 07:09:46 -0300

    cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ conn.sync()
 app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(cors({
-  origin: 'https://neocities.org'
+  origin: 'https://lyricaltokarev.neocities.org'
 }));
 
 cloudReq(app)
