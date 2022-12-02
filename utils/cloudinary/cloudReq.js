f86a3600795e869c36404c52fff593f2d557e453
Widenyo <misterhomunculo@gmail.com>
2022-12-02 01:46:35 -0300

    cloudreq fix

--- utils/cloudinary/cloudReq.js
+++ utils/cloudinary/cloudReq.js
@@ -2,14 +2,14 @@ const cloudinary = require('../../configs/cloudinary')
 
 async function cloudReq(app){
     try{
-      const gifsReq = await cloudinary.v2.search.expression( 'resource_type:image').sort_by('public_id','desc').max_results(500).execute()
+      const gifsReq = await cloudinary.v2.search.expression( 'resource_type:image AND folder=gifs-epicos').sort_by('public_id','desc').max_results(500).execute()
       let gifs = gifsReq.resources
      
       if(gifsReq.next_cursor){
          let flag = true
          let pointer = gifsReq.next_cursor
          while(flag){
-             const reqGifs = await cloudinary.v2.search.expression( 'resource_type:image').sort_by('public_id','desc').max_results(500).next_cursor(pointer).execute()
+             const reqGifs = await cloudinary.v2.search.expression( 'resource_type:image AND folder=gifs-epicos').sort_by('public_id','desc').max_results(500).next_cursor(pointer).execute()
              gifs = [...gifs, ...reqGifs.resources]
              pointer = reqGifs.next_cursor
              if(!pointer) flag = false
