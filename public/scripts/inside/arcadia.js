d7fa28a7906b76b9ccd28e5b48a1233f0f496738
Widenyo <misterhomunculo@gmail.com>
2023-07-26 00:56:50 -0300

    modified letters event and background

--- public/scripts/inside/arcadia.js
+++ public/scripts/inside/arcadia.js
@@ -1,17 +1,11 @@
 let current = 1
 
-addEventWithTimeout()
+document.getElementById('text'+current).addEventListener('mouseover', mouseOverHandler)
 
 function mouseOverHandler(){
     current++
     document.getElementById('text'+current).style.animationPlayState = "running"
     document.getElementById('text'+current).style.visibility = "visible"
     document.getElementById('text'+(current-1)).removeEventListener('mouseover', mouseOverHandler)
-    if(current < 4) addEventWithTimeout()
-}
-
-function addEventWithTimeout(){
-    setTimeout(() => {
-        document.getElementById('text'+current).addEventListener('mouseover', mouseOverHandler)
-    }, 1730)
+    if(current < 4) document.getElementById('text'+current).addEventListener('mouseover', mouseOverHandler)
 }
